5c269860c0c5a0d10c5c23e9cc486240a6a5aaf5
Steve Green <stevegreen22@me.com>
2025-08-29 19:02:15 +0100

    map tweaks

--- assets/maps/tmx/terrain.tsx
+++ assets/maps/tmx/terrain.tsx
@@ -1,14 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="terrain" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="img/terrain.png" width="1024" height="1024"/>
- <tile id="0">
-  <properties>
-   <property name="enemy" value="1"/>
-  </properties>
- </tile>
- <tile id="478">
-  <properties>
-   <property name="collision" value="1"/>
-  </properties>
- </tile>
+<tileset version="1.10" tiledversion="1.11.2" name="terrain" tilewidth="32" tileheight="32" tilecount="2048" columns="64">
+ <image source="img/terrain.png" width="2048" height="1024"/>
 </tileset>
