328ad3cc8fbf82d95a06d6214dbe5dc09ea15ea2
Steve Green <stevegreen22@me.com>
2025-09-02 16:20:52 +0100

    updates

---
+++ assets/maps/tmx/monster_spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="monster_spritesheet" tilewidth="32" tileheight="32" tilecount="54" columns="6">
+ <image source="../../characters/enemy/monster_spritesheet.png" width="192" height="288"/>
+</tileset>
