7a2c601db73b7dd323de4ff9982d5916659e2c25
Steve Green <stevegreen22@me.com>
2025-09-02 08:55:11 +0100

    minor tweaks

---
+++ assets/maps/tmx/animal_spritesheet.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="animal_spritesheet" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../../animals/animal_spritesheet.png" width="320" height="320"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="sheep"/>
+   <property name="type" value="animal"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="name" value="chicken"/>
+   <property name="type" value="animal"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <animation>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="name" value="rabbit"/>
+   <property name="type" value="animal"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="name" value="guinea_pig"/>
+   <property name="type" value="animal"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="name" value="pig"/>
+   <property name="type" value="animal"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="name" value="capybara"/>
+   <property name="type" value="animal"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="name" value="hedgehog"/>
+   <property name="type" value="animal"/>
+  </properties>
+ </tile>
+</tileset>
